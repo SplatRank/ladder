cc6449eec782682db7f1fec36780dfa4c8bc4e79
yukidaruma <yukidaruma@users.noreply.github.com>
2021-05-16 20:53:51 +0900

    feat: Set up Chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'next-auth/client';
 import { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <ChakraProvider>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </ChakraProvider>
   );
 };
 
