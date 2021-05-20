4fe41c7dc1adfa91e0991053fb11b2e98aeab987
yukidaruma <yukidaruma@users.noreply.github.com>
2021-05-21 00:48:06 +0900

    feat: Add `role` mechanism

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Role } from '@prisma/client';
 
 declare global {
   namespace NodeJS {
@@ -10,5 +10,29 @@ declare global {
       TWITTER_CLIENT_ID: string;
       TWITTER_CLIENT_SECRET: string;
     }
+  }
+}
+
+declare module 'next-auth' {
+  export interface User {
+    email?: string | null;
+    image?: string | null;
+    name?: string | null;
+    role: Role;
+  }
+
+  export interface Session {
+    user?: {
+      email?: string | null;
+      image?: string | null;
+      name?: string | null;
+      roles?: Role[];
+    };
+  }
+}
+
+declare module 'next-auth/jwt' {
+  export interface JWT {
+    roles?: Role[];
   }
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,17 @@
+import { useRadio } from '@chakra-ui/radio';
 import { NextApiHandler } from 'next';
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthOptions, User } from 'next-auth';
 import { Prisma as PrismaAdapter } from 'next-auth/adapters';
+import { JWT } from 'next-auth/jwt';
 import Providers from 'next-auth/providers';
 import prisma from '@/prisma';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
 
-const options = {
+const isUserType = (user: JWT | User): user is User => Boolean(user.role);
+
+const options: NextAuthOptions = {
   providers: [
     Providers.Twitter({
       clientId: process.env.TWITTER_CLIENT_ID,
@@ -16,4 +20,23 @@ const options = {
   ],
   adapter: PrismaAdapter.Adapter({ prisma }),
   secret: process.env.SECRET,
+  callbacks: {
+    async jwt(token, user, _account, _profile, _isNewUser) {
+      if (user) {
+        token.roles = [user.role];
+      }
+      return token;
+    },
+    async session(session, user) {
+      if (session.user) {
+        if (isUserType(user)) {
+          session.user.roles = [user.role];
+        } else if (user) {
+          session.user.roles = user.roles;
+        }
+      }
+
+      return session;
+    },
+  },
 };
